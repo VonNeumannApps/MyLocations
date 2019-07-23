59db85d3f0688c8503d726fdee7c616f62d1a535
VonNeumannApps <mauro81@gmail.com>
2019-07-23 10:10:58 +0200

    Rimettiamo main activity come launcher ( per aprirci lo spash sopradopo) chiudiamo lo splash dopo 2 sec con un handler

--- app/src/main/java/it/vonneumannapps/mylocations/SplashActivity.java
+++ app/src/main/java/it/vonneumannapps/mylocations/SplashActivity.java
@@ -3,12 +3,23 @@ package it.vonneumannapps.mylocations;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
     }
 }
