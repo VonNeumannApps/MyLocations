f9972c247fb3a32bc8fda4cebbd715cbdf217e47
VonNeumannApps <mauro81@gmail.com>
2019-07-23 09:30:52 +0200

    Initial commit

---
+++ app/src/main/java/it/vonneumannapps/mylocations/MainActivity.java
@@ -0,0 +1,16 @@
+package it.vonneumannapps.mylocations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // questa immagine descrive i luoghi visitati, con indirizzo e immagine
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
