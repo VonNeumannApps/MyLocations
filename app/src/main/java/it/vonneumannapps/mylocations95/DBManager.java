8e16a077cbef3bdb83b69a0e2c51c498ff456981
VonNeumannApps <mauro81@gmail.com>
2019-07-23 14:25:29 +0200

    Aggiunto db manager

---
+++ app/src/main/java/it/vonneumannapps/mylocations95/DBManager.java
@@ -0,0 +1,67 @@
+package it.vonneumannapps.mylocations95;
+
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class DBManager extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "db";
+    public static final int DATABASE_VERSION = 1;
+
+    public DBManager(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        String query = "CREATE TABLE luoghi (id PRIMARY KEY AUTOINCREMENT, descrizione TEXT, indirizzo TEXT, immagine BLOB)";
+
+        sqLiteDatabase.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public ArrayList<Bundle> getLocations() {
+
+        SQLiteDatabase db = getReadableDatabase();
+
+        ArrayList<Bundle> locations = new ArrayList<>();
+
+        String query = "SELECT * FROM luoghi";
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        cursor.moveToFirst();
+
+        while (!cursor.isAfterLast()) {
+
+            Bundle location = new Bundle();
+
+            location.putInt("id", cursor.getInt(cursor.getColumnIndex("id")));
+            location.putString("descrizione", cursor.getString(cursor.getColumnIndex("descrizione")));
+            location.putString("indirizzo", cursor.getString(cursor.getColumnIndex("indirizzo")));
+            location.putByteArray("immagine", cursor.getBlob(cursor.getColumnIndex("immagine")));
+
+            locations.add(location);
+
+            cursor.moveToNext();
+        }
+
+        db.close();
+
+        return locations;
+    }
+
+
+}
+
