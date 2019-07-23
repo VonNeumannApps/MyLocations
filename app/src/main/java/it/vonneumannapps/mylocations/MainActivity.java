eb90e950b2455026a1087d274c212f28e586a79e
VonNeumannApps <mauro81@gmail.com>
2019-07-23 10:12:54 +0200

    Apriamo lo splash dalla MainActivity

--- app/src/main/java/it/vonneumannapps/mylocations/MainActivity.java
+++ app/src/main/java/it/vonneumannapps/mylocations/MainActivity.java
@@ -2,6 +2,7 @@ package it.vonneumannapps.mylocations;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,10 +10,18 @@ public class MainActivity extends AppCompatActivity {
     // questa immagine descrive i luoghi visitati, con indirizzo e immagine
 
     // lo splash
+    void openSplashActivity() {
+
+        Intent intent = new Intent(MainActivity.this, SplashActivity.class);
+
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openSplashActivity();
     }
 }
