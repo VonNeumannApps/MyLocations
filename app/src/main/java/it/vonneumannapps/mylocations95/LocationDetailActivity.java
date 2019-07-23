55925f9309da9b5595506b1f1cde524c671de6af
VonNeumannApps <mauro81@gmail.com>
2019-07-23 14:34:06 +0200

    Aggiunta nuova LocationDetailActivity

---
+++ app/src/main/java/it/vonneumannapps/mylocations95/LocationDetailActivity.java
@@ -0,0 +1,14 @@
+package it.vonneumannapps.mylocations95;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_detail);
+    }
+}
