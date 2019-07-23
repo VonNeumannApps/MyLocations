1ac7d74b81dd35478f68bcc4e0dbc42c352afc1d
VonNeumannApps <mauro81@gmail.com>
2019-07-23 09:35:15 +0200

    Aggiunta SlashActivity per fare lo splash dal launcer quando parte l'app

--- app/src/main/java/it/vonneumannapps/mylocations/SplashActivity.java
+++ app/src/main/java/it/vonneumannapps/mylocations/SplashActivity.java
@@ -4,15 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-    // questa immagine descrive i luoghi visitati, con indirizzo e immagine
-
-    // lo splash
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
