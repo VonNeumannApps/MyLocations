922f24d7a6de2d8fc5866a0ef5fde1e38a094761
VonNeumannApps <mauro81@gmail.com>
2019-07-24 11:25:17 +0200

    Aggiunto utilizzo delle fotocamera per fare una foto. Aggiunta gestione file per Android Oreo e versioni precedenti.

--- app/src/main/java/it/vonneumannapps/mylocations95/LocationDetailActivity.java
+++ app/src/main/java/it/vonneumannapps/mylocations95/LocationDetailActivity.java
@@ -1,20 +1,34 @@
 package it.vonneumannapps.mylocations95;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+
 import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class LocationDetailActivity extends AppCompatActivity {
+import java.io.File;
+
+public class LocationDetailActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
     EditText descET;
     EditText addressET;
@@ -25,7 +39,7 @@ public class LocationDetailActivity extends AppCompatActivity {
 
     Bundle location;
 
-    String[] actions = {getString(R.string.CAMERA_ACTION_TEXT), getString(R.string.GALLERY_ACTION_TEXT)};
+    String[] actions;
 
     BaseAdapter actionsAdapter;
 
@@ -65,6 +79,46 @@ public class LocationDetailActivity extends AppCompatActivity {
         };
     }
 
+    void checkForCameraPermission() {
+
+        int myCameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if(myCameraPermission != PackageManager.PERMISSION_GRANTED){
+
+            String[] permissionsToRequest = {Manifest.permission.CAMERA};
+            ActivityCompat.requestPermissions(this, permissionsToRequest, Utils.CAMERA_PERMISSION_CODE);
+        }
+        else {
+            takePicture();
+        }
+    }
+
+    void takePicture() {
+
+        // SCATTA FOTO
+
+        // DOUBT: difference btw this and LocationDetailActivity.this
+        Uri outputFileUri = Utils.getOutputFileUri(this);
+
+        //crete intent to take a picture and return control
+        // to the calling application
+        // apre la fotocamera
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // create a file to save the  image
+        // extra con il percorso del file
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+        //start the image capture intent
+        startActivityForResult(intent, Utils.TAKE_PICTURE_CODE);
+
+        /*
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            // Do something for Oreo 26 and above versions
+        } else{
+            // do something for phones running an SDK before lollipop
+        }*/
+    }
+
     void openActionSelectionDialog() {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -74,7 +128,18 @@ public class LocationDetailActivity extends AppCompatActivity {
         builder.setAdapter(actionsAdapter, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                //TODO
+
+                final int FOTOCAMERA = 0;
+                //final int GALLERIA = 1;
+
+                if(i==FOTOCAMERA) {
+
+                    //TODO
+                    checkForCameraPermission();// controlla permessi e poi apre fotocamera
+                }
+                else { //GALLERIA
+                    //TODO
+                }
             }
         });
 
@@ -85,13 +150,60 @@ public class LocationDetailActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case Utils.CAMERA_PERMISSION_CODE: {
+
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePicture();
+                }
+                else {
+                    //TODO messaggio all'utente: servono i permessi
+                }
+            }
+            break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        switch (requestCode) {
+
+            case Utils.TAKE_PICTURE_CODE : {
+
+                if(resultCode == RESULT_OK) {
+
+                    String pathName = Utils.getMainDirectory(this).getPath() + Utils.TMP_FILENAME;
+                    File tempFile = new File(pathName);
+                    Bitmap bitmap = BitmapFactory.decodeFile(tempFile.getPath());
+
+                    imageIV.setImageBitmap(bitmap);
+                }
+
+                break;
+            }
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_detail);
 
+        this.actions = new String[] {getString(R.string.CAMERA_ACTION_TEXT), getString(R.string.GALLERY_ACTION_TEXT)};
+
         initializeActionsAdapter();
 
-        dbManager = new DBManager(this, DBManager.DATABASE_NAME, null, DBManager.DATABASE_VERSION);
+                dbManager = new DBManager(this, DBManager.DATABASE_NAME, null, DBManager.DATABASE_VERSION);
 
         descET = findViewById(R.id.descriptionEditText);
         addressET = findViewById(R.id.addressEditText);
@@ -114,15 +226,12 @@ public class LocationDetailActivity extends AppCompatActivity {
         });
 
         ImageView addImageButton = findViewById(R.id.addImageButton);
-
         addImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                openActionSelectionDialog();//scelta se fotocamera o galleria
             }
         });
 
-
     }
-
 }
--- app/src/main/java/it/vonneumannapps/mylocations95/Utils.java
+++ app/src/main/java/it/vonneumannapps/mylocations95/Utils.java
@@ -1,11 +1,22 @@
 package it.vonneumannapps.mylocations95;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;// used for os version checks
+
+import androidx.core.content.FileProvider;
+
+import java.io.File;
 
 public class Utils {
 
     public static final int ADD_OR_EDIT_CODE = 100;
+    public static final int TAKE_PICTURE_CODE = 200;
+    public static final int CAMERA_PERMISSION_CODE = 300;
+
+    public static final String TMP_FILENAME = "/temp.jpg";
 
     public static Bitmap convertByteArrayToBitmap(byte[] bytes) {
 
@@ -13,5 +24,32 @@ public class Utils {
 
         return bmp;
 
+    }
+
+    public static File getMainDirectory(Context context) {
+
+
+        return context.getExternalFilesDir(null);
+    }
+
+    public static Uri getOutputFileUri(Context context) {
+
+        // Creo l'URI di destinazione, file (cartella_corrente)/temp.jpg
+        File dir = Utils.getMainDirectory(context);
+        File file = new File(dir + TMP_FILENAME );
+
+        Uri outputFileUri;
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){ // Android Oreo 8.0 and avove
+
+            String authority = BuildConfig.APPLICATION_ID + ".provider";
+            outputFileUri = FileProvider.getUriForFile(context, authority, file);
+
+        } else{
+            // for phones running an SDK before Oreo 8.0
+            outputFileUri = Uri.fromFile(file);
+        }
+
+        return outputFileUri;
     }
 }
